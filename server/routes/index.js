fb0884d45ffa1d8b233cbabfb3b52a3183879299
Doug Cole <idugcoal@gmail.com>
2016-07-13 20:40:24 -0700

    adds sign in with twitter

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var Twitter = require('node-twitter-api');
+var secret = require('../../secret');
 
-router.get('/', function(req, res) {
-	res.send("<h1>testing</h1>");
+var twitter = new Twitter({
+  consumerKey: secret.twitter.consumerKey,
+  consumerSecret: secret.twitter.consumerSecret,
+  callback: secret.twitter.callbackUrl
 })
+
+var _requestSecret;
+
+router.get('/testing', function(req, res) {
+	res.send("<h1>testing is neato</h1>");
+})
+
+router.get("/request-token", function(req, res) {
+  console.log('in request token');
+  twitter.getRequestToken(function(err, requestToken, requestSecret) {
+    if(err) {
+      res.status(500).send(err);
+    } else {
+      _requestSecret = requestSecret;
+      res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + requestToken);
+    }
+  });
+});
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,41 +2,13 @@ var express = require('express');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpack = require('webpack');
 var webpackConfig = require('../webpack.config.js');
-var app = express();
-
 var routes = require('./routes/index');
 
+var app = express();
 var compiler = webpack(webpackConfig);
 
 app.use('/', routes);
-
 app.use(express.static('./www'));
-
-var Twitter = require('node-twitter-api');
-var secret = require('../secret');
-console.log('secret', secret);
-
-module.exports = function(app) {
-  var twitter = new Twitter({
-    consumerKey: secret.twitter.consumerKey,
-    consumerSecret: secret.twitter.consumerSecret,
-    callback: secret.twitter.callbackUrl
-  })
-
-  var _requestSecret;
-
-  app.get("/request-token", function(req, res) {
-    twitter.getRequestToken(function(err, requestToken, requestSecret) {
-      if(err) {
-        res.status(500).send(err);
-      } else {
-        _requestSecret = requestSecret;
-        res.redirect("https://api.twitter.com/oauth/authenticate?oauth_token=" + requestToken);
-      }
-    });
-  });
-};
-
 app.use(webpackDevMiddleware(compiler, {
   hot: true,
   filename: 'bundle.js',
@@ -47,14 +19,8 @@ app.use(webpackDevMiddleware(compiler, {
   historyApiFallback: true,
 }));
 
-// var server = app.listen(3000, 'localhost', function() {
-//   var host = server.address().address;
-//   var port = server.address().port;
-//   console.log('Example app listening at http://%s:%s', host, port);
-// });
-
 app.set('port', process.env.PORT || 8080);
 
 var server = app.listen(app.get('port'), function() {
-  console.log("app started");
+  console.log("app listening at http://localhost:8080");
 });
