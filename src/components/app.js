1cca15db232a14fe9f088aa222ba462a45b369d5
Doug Cole <idugcoal@gmail.com>
2016-07-21 18:52:09 -0700

    removes most console.logs

--- src/components/app.js
+++ src/components/app.js
@@ -11,12 +11,9 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {showTweets: false, username: '', tweetList: []}
-
-    console.log('in app constructor', props, this.state)
   }
 
   getUsername(username) {
-    console.log('in app', username, this.state, this.props);
     var reqConfig = {
       headers: {
         'Content-Type': 'x-www-form-urlencoded',
@@ -28,9 +25,6 @@ export default class App extends Component {
       axios.post('http://localhost:8080/getTweets', {username: username}, reqConfig)
         .then((result) => {
           this.setState({tweetList: result.data})
-          console.log('in tweets, result.data', result.data)
-          console.log('in tweets,this.state', this.state)
-
          });  
   }
   
--- src/components/tweet.js
+++ src/components/tweet.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
-import Toggle from 'material-ui/Toggle';
+import {Card, CardHeader, CardText} from 'material-ui/Card';
 import DateDisplay from './dateDisplay'
 
 export default class Tweet extends Component {
@@ -12,21 +10,21 @@ export default class Tweet extends Component {
 
   render() {
     return (
-    <span>
-      <Card>
-        <CardHeader
-          title={this.props.tweet.user.screen_name}
-          subtitle={this.props.tweet.user.name}
-          avatar={this.props.tweet.user.profile_image_url}
-          actAsExpander={false}
-          showExpandableButton={false}
-        />
-        <CardText>
-          {this.props.tweet.text}
-          <DateDisplay date={this.props.tweet.created_at} />
-        </CardText>
-      </Card>
-    </span>
+      <span>
+        <Card>
+          <CardHeader
+            title={this.props.tweet.user.screen_name}
+            subtitle={this.props.tweet.user.name}
+            avatar={this.props.tweet.user.profile_image_url}
+            actAsExpander={false}
+            showExpandableButton={false}
+          />
+          <CardText>
+            {this.props.tweet.text}
+            <DateDisplay date={this.props.tweet.created_at} />
+          </CardText>
+        </Card>
+      </span>
 		)
 	}
 }
